f4a1ce20aac1ade044adddb4d40055c7dc731a51
SergeyB19 <my.h.m@yandex.ru>
2024-02-07 12:10:32 +0300

    Consumer

--- src/main/java/com/zaurtregulov/nested_classes/static_nested_class/lyambda/Test3.java
+++ src/main/java/com/zaurtregulov/nested_classes/static_nested_class/lyambda/Test3.java
@@ -1,6 +1,7 @@
 package com.zaurtregulov.nested_classes.static_nested_class.lyambda;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Test3 {
@@ -12,8 +13,19 @@ public class Test3 {
         return al;
     }
 
+    public static void changeCar(Car car, Consumer<Car> carConsumer) {
+        carConsumer.accept(car);
+    }
+
     public static void main(String[] args) {
         ArrayList<Car> ourCars = createThreeCars(() -> new Car("Nissan Tiida", "Silver Metalic", 1.6));
+        System.out.println("Our cars: " + ourCars);
+
+        changeCar(ourCars.get(0), car -> {
+            car.color = "red";
+            car.engine = 2.4;
+            System.out.println("Upgraded car: " + car);
+        });
         System.out.println("Our cars: " + ourCars);
     }
 }
