880679d1d14a300f99505f55c33e9d45f293e8d7
SergeyB19 <my.h.m@yandex.ru>
2024-02-10 18:33:49 +0300

    Streams(method sorted)

---
+++ src/main/java/com/zaurtregulov/nested_classes/static_nested_class/stream/Test5.java
@@ -0,0 +1,11 @@
+package com.zaurtregulov.nested_classes.static_nested_class.stream;
+
+import java.util.Arrays;
+
+public class Test5 {
+    public static void main(String[] args) {
+        int[] array = {3, 8, 1, 5, 9, 12, 4, 21, 81, 7, 18};
+        array = Arrays.stream(array).sorted().toArray();
+        System.out.println(Arrays.toString(array));
+    }
+}
