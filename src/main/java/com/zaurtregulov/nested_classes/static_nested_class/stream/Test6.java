938a233c546e38a2296b3b9efd6bfa0466ab938f
SergeyB19 <my.h.m@yandex.ru>
2024-02-11 10:10:13 +0300

    Streams(method concat)

--- src/main/java/com/zaurtregulov/nested_classes/static_nested_class/stream/Test6.java
+++ src/main/java/com/zaurtregulov/nested_classes/static_nested_class/stream/Test6.java
@@ -12,5 +12,12 @@ public class Test6 {
             System.out.println("!!!");
             return el % 2 == 0;
         }).collect(Collectors.toList());
+
+
+        Stream<Integer> stream2 = Stream.of(1, 2, 3, 4, 5);
+        Stream<Integer> stream3 = Stream.of(6, 7, 8, 9, 10);
+        Stream<Integer> stream4 = Stream.concat(stream2, stream3);
+        stream4.forEach(System.out::println);
+
     }
 }
